5d0fea0e8966ab07efb2cea84b821297ea0d657c
ambarbueno20 <158345732+ambarbueno20@users.noreply.github.com>
2024-04-22 10:30:14 -0400

    Add files via upload

---
+++ Capitulo 3/Programa 3.13.c/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* Fibonacci.
+El prpgrma calcula y escibe los primeros 50 numeros de fibonacci.
+
+I,PRI, SEG, SIG; variables de tipo entero. */
+
+void main(void)
+{
+int I, PRI = 0, SEG = 1, SIG;
+printf("/t %d /t%d" , PRI, SEG);
+for (I= 3; I<= 50; I++)
+{
+    SIG = PRI + SEG;
+    PRI = SEG;
+    SEG = SIG;
+    printf("/t%d", SIG);
+}
+}
+
