001612d9644130620753ef106a9f2a348faad288
ambarbueno20 <158345732+ambarbueno20@users.noreply.github.com>
2024-04-22 10:48:06 -0400

    Add files via upload

---
+++ Capitulo 7.2/Programa 7.16.c/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#/*Decodifica.
+El programa decodifica una cadena de caracteres compuesta por numeros y
+letras. */
+
+void interpreta(char *);     /*Prototipo de funcion. */
+
+void main(void)
+{
+char cad[50];
+printf("\nIngrese la cadena de caracteres:");
+gets(cad);
+interpreta(cad);
+}
+
+void interpreta(char *cadena)
+/*Esta función se utilzia para decodificar la cadena de caracteres. */
+{
+int i = 0, j, k;
+while (cad[i] != '\0')
+{
+    if (isalpha (cad[i]))   /*Se utiliza para observar si el caracter es una letra. */
+
+    {
+        k = cad[i-1]-48;
+        /* En la variable entera k se almacena el ascii del numero —convertido
+    en caracter— que nos interesa, menos 48 que corresponde al ascii
+    del digito 0. */
+        for (j = 0; j < k; j++)
+            putchar(cad[i]);
+    }
+    i++;
+}
+}
